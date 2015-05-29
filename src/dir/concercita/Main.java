703055f8bc9baf578eb825fc278f5455f6bfea66
Juan José <jjlabradorglez@gmail.com>
2015-05-29 19:41:49 +0100

    prueba

--- src/dir/concercita/Main.java
+++ src/dir/concercita/Main.java
@@ -2,6 +2,10 @@ package dir.concercita;
 
 public class Main {
 
+	/**
+	 * @author Juan José
+	 * Prueba 
+	 */
 	public static void main(String[] args) {
 		new ConcercitaForm().setVisible(true);
 	}
