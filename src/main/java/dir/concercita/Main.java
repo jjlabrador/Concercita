35ca8c3f18b876b444eaf8dd4ab5b6b9bdcb2be1
Jonay <jhonsay@gmail.com>
2015-05-30 12:50:33 +0100

    Quitar @Author del Main()

--- src/main/java/dir/concercita/Main.java
+++ src/main/java/dir/concercita/Main.java
@@ -2,10 +2,6 @@ package dir.concercita;
 
 public class Main {
 
-	/**
-	 * @author Juan José
-	 * Prueba 
-	 */
 	public static void main(String[] args) {
 		new ConcercitaForm().setVisible(true);
 	}
