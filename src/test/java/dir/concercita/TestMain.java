8634a85337bd1e3a21491cc1b02a9ba70dd38280
Jonay <jhonsay@gmail.com>
2015-05-30 14:55:19 +0100

    Cambios en Testfile

--- src/test/java/dir/concercita/TestMain.java
+++ src/test/java/dir/concercita/TestMain.java
@@ -1,14 +1,14 @@
 package dir.concercita;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import java.lang.String;
 
 public class TestMain {
 
 	@Test
-	public void TestMain() {
+	public String TestMain() {
 		return("Not yet implemented");
 	}
 
