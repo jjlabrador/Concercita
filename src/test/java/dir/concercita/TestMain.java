29f21346b70bd66ed3a7dfd1db6413a2bfc9ff68
Jonay <jhonsay@gmail.com>
2015-05-30 15:09:10 +0100

    Cambios en Testfile

--- src/test/java/dir/concercita/TestMain.java
+++ src/test/java/dir/concercita/TestMain.java
@@ -1,5 +1,5 @@
 package dir.concercita;
-
+import static dir.concercita.ConcercitaForm;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -9,6 +9,7 @@ public class TestMain {
 
 	@Test
 	public void TestMain() {
+		new ConcercitaForm().setVisible(true);
 		assertTrue(1 == 1); 
 	}
 
